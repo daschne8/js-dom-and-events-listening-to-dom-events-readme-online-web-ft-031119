0cd2fb6eae889af26cae9ffeb467ce591bdb6486
Daniel Schneider <daschne8@gmail.com>
2019-06-18 14:02:35 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 const input = document.querySelector('input');
- 
+
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
 });
